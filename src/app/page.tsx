053817ee1136ab28f0304de5550059b2cf85e150
kcselm <kcselm93@gmail.com>
2024-12-10 15:24:32 -0800

    display posts and force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 import Link from "next/link";
 import TopNav from "./components/TopNav";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/43a34a54-b036-4ac8-b7e2-890e34901d37-1t8ncw.jpeg",
@@ -9,18 +10,26 @@ const mockUrls = [
   "https://utfs.io/f/43a34a54-b036-4ac8-b7e2-890e34901d37-1t8ncw.jpeg",
 ];
 
+export const dynamic = "force-dynamic";
+
 const mockImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log("posts: ", posts);
+
   return (
     <main>
       <TopNav />
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((img) => (
-          <div key={img.url} className="w-48 p-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((img, index) => (
+          <div key={img.id + index} className="w-48 p-4">
             <img src={img.url} alt={img.url}></img>
           </div>
         ))}
