54954eb3f53fc45ba33d38ebb0c253dffdf4b77f
kcselm <kcselm93@gmail.com>
2024-12-10 17:51:51 -0800

    Use auth for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,41 @@ import { imageConfigDefault } from "next/dist/shared/lib/image-config";
 import Link from "next/link";
 import TopNav from "./_components/TopNav";
 import { db } from "~/server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
+
+async function Images() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((img, index) => (
+        <div key={img.id + index} className="w-48 p-4">
+          <img src={img.url} alt={img.name}></img>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default async function HomePage() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
-  console.log("images: ", images);
 
   return (
     <main>
       <TopNav />
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((img, index) => (
-          <div key={img.id + index} className="w-48 p-4">
-            <img src={img.url} alt={img.name}></img>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full pt-6 text-center text-2xl">
+          Please sign in
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
